91611298c35993128a7c399c9db457042ea1875b
Aleksandr Kasianenko <saix61@yandex.ru>
2024-08-08 02:59:00 +0300

    docker

--- pcrm-front/vite.config.ts
+++ pcrm-front/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 8080,
     proxy: {
       "/api": {
         target: "http://localhost:3000", // адрес вашего бэкенда
